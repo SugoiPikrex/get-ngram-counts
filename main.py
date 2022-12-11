e191b46758e69af29d10d057c170bbd0dfb558e9
Mahdiazhari <mahdi.austian@gmail.com>
2022-12-11 12:43:23 +0100

    fix python indentations

--- main.py
+++ main.py
@@ -25,14 +25,6 @@ import itertools
 
 WORKING_DIR = '/content/drive/MyDrive/generate_n_grams'
 
-def generate_directories(working_directory = WORKING_DIR):
-    """
-    Generates the required directories in the WORKING DIR
-    """
-  dir_name= WORKING_DIR + '/ngrams_multiprocessing_nosave_benchmark/'
-  for i in range(2,7):
-    os.makedirs(dir_name + str(i))
-    
 
 def list_files(dir):
     """
@@ -45,9 +37,20 @@ def list_files(dir):
         if (len(files) > 0):                                                                                          
             for file in files:                                                                                        
                 r.append(os.path.join(subdir, file))                                                                         
-    return r   
+    return r
 
-def get_counts(counter):
+
+def generate_directories(working_directory = WORKING_DIR):
+    """
+    Generates the required directories in the WORKING DIR
+    """
+    dir_name= WORKING_DIR + '/ngrams_multiprocessing_nosave_benchmark/'
+  
+    for i in range(2,7):
+      os.makedirs(dir_name + str(i))
+    
+
+def get_counts(counter_file):
     """Get counts as a dictionary from a counter.
 
     Args:
@@ -56,13 +59,14 @@ def get_counts(counter):
     Returns:
         dict: reference_dict containing the ngrams
     """
-  reference_dict = {}
-  for item in counter.most_common():
-    text= ' '.join(item[0])
-    count = item[1]
-    if not reference_dict.get(text,""):
-      reference_dict[text] = count
-  return reference_dict
+    reference_dict = {}
+  
+    for item in counter_file.most_common():
+      text= ' '.join(item[0])
+      count = item[1]
+      if not reference_dict.get(text,""):
+        reference_dict[text] = count
+    return reference_dict
 
 
 def count_in_text_local(batch_num, text, dir_name_template, i):
@@ -74,12 +78,12 @@ def count_in_text_local(batch_num, text, dir_name_template, i):
         dir_name_template (str): template for the directory name
         i (int): the n-th gram from 2-6 grams
     """
-  ngram_counts = Counter(ngrams(text.split(), i))
-  temp_dict = get_counts(ngram_counts)
-  dir_name = dir_name_template + str(i)
-  with open(dir_name + '/{}.json'.format(batch_num), 'w') as fp:
-    json.dump(temp_dict, fp)
-  print('saved batch {batch_num} for {i} grams'.format(batch_num=batch_num, i=i))
+    ngram_counts = Counter(ngrams(text.split(), i))
+    temp_dict = get_counts(ngram_counts)
+    dir_name = dir_name_template + str(i)
+    with open(dir_name + '/{}.json'.format(batch_num), 'w') as fp:
+      json.dump(temp_dict, fp)
+    print('saved batch {batch_num} for {i} grams'.format(batch_num=batch_num, i=i))
 
 def get_ngram_dicts(filename, save_dir):
     """Get ngram counts as a folder of dictionaries from a given text file.
@@ -90,53 +94,56 @@ def get_ngram_dicts(filename, save_dir):
         filename (str): Directory of the corpus file
         save_dir (str): Directory to save the file
     """
-  #ngram_dicts = {2: [], 3:[], 4:[], 5:[], 6:[]}
-  with open(filename) as fp:
-      text = ''
-      count = 0
-      segment_number = 1
-      grams_n = [2,3,4,5,6]
-      while True:
-        line = fp.readline()
-        if not line:
-          if segment_number == 1:
-            file_name = 'total_n_gram'
-            params = [(file_name, text, save_dir, num) for num in grams_n]
-            with Pool(processes=8) as pool:
-              pool.starmap(count_in_text_local, params)
-            #count_in_text_local('total_n_gram', text ,save_dir)
-          else:
-            file_name = segment_number
-            params = [(file_name, text, save_dir, num) for num in grams_n]
-            with Pool(processes=8) as pool:
-              pool.starmap(count_in_text_local, params)
-            #count_in_text_local(segment_number, text ,save_dir)
-          #process_files_dict(save_dir)
-          break
-        text += line
-        count += 1
-        if count == 100000:
-          if segment_number == 1:
-            file_name = 'total_n_gram'
-            params = [(file_name, text, save_dir, num) for num in grams_n]
-            with Pool(processes=8) as pool:
-              pool.starmap(count_in_text_local, params)
-            #count_in_text_local('total_n_gram', text ,save_dir)
-          else:
-            file_name = segment_number
-            params = [(file_name, text, save_dir, num) for num in grams_n]
-            with Pool(processes=8) as pool:
-              pool.starmap(count_in_text_local, params)
-            #count_in_text_local(segment_number, text ,save_dir)
-          #process_files_dict(save_dir)
-          count = 0
-          segment_number += 1 
-          text = ''
+    #ngram_dicts = {2: [], 3:[], 4:[], 5:[], 6:[]}
+    with open(filename) as fp:
+        text = ''
+        count = 0
+        segment_number = 1
+        grams_n = [2,3,4,5,6]
+        while True:
+          line = fp.readline()
+          if not line:
+            if segment_number == 1:
+              file_name = 'total_n_gram'
+              params = [(file_name, text, save_dir, num) for num in grams_n]
+              with Pool(processes=8) as pool:
+                pool.starmap(count_in_text_local, params)
+              #count_in_text_local('total_n_gram', text ,save_dir)
+            else:
+              file_name = segment_number
+              params = [(file_name, text, save_dir, num) for num in grams_n]
+              with Pool(processes=8) as pool:
+                pool.starmap(count_in_text_local, params)
+              #count_in_text_local(segment_number, text ,save_dir)
+            #process_files_dict(save_dir)
+            break
+          text += line
+          count += 1
+          if count == 100000:
+            if segment_number == 1:
+              file_name = 'total_n_gram'
+              params = [(file_name, text, save_dir, num) for num in grams_n]
+              with Pool(processes=8) as pool:
+                pool.starmap(count_in_text_local, params)
+              #count_in_text_local('total_n_gram', text ,save_dir)
+            else:
+              file_name = segment_number
+              params = [(file_name, text, save_dir, num) for num in grams_n]
+              with Pool(processes=8) as pool:
+                pool.starmap(count_in_text_local, params)
+              #count_in_text_local(segment_number, text ,save_dir)
+            #process_files_dict(save_dir)
+            count = 0
+            segment_number += 1 
+            text = ''
 
 
 def main():
-  dir_name = WORKING_DIR + '/ngrams_multiprocessing_nosave_benchmark/'
-  get_ngram_dicts(file_name, dir_name)
+    dir_name = WORKING_DIR + '/ngrams_multiprocessing_nosave_benchmark/'
+    file_name =  WORKING_DIR + "/wikimatrix.en" #or insert filename here
+    get_ngram_dicts(file_name, dir_name)
+
+
 
 #Start the process
 start = time.time()
